d23ef7518f5a149c51175f1e0f943f0481758943
homoludens <marko.homoludens@gmail.com>
2011-04-27 19:09:18 +0200

    removing commented code

--- Ui_mainwindow.py
+++ Ui_mainwindow.py
@@ -120,72 +120,29 @@ class MainUi(object):
         MainWindow.setWindowIcon(QtGui.QIcon("images/musicstore.png"))
 
         self.centralwidget = QtGui.QWidget(MainWindow)
-        #self.centralwidget.setObjectName("centralwidget")
 
         self.gridlayout = QtGui.QGridLayout(self.centralwidget)
-        #self.gridlayout.setObjectName("gridlayout")
 
         self.tabWidget = QtGui.QTabWidget(self.centralwidget)
-        #self.tabWidget.setObjectName("tabWidget")
 
         self.tab = QtGui.QWidget()
-        #self.tab.setObjectName("tab")
 
         self.gridlayout1 = QtGui.QGridLayout(self.tab)
-        #self.gridlayout1.setObjectName("gridlayout1")
 
         self.splitter_2 = QtGui.QSplitter(self.tab)
         self.splitter_2.setOrientation(QtCore.Qt.Horizontal)
-        #self.splitter_2.setObjectName("splitter_2")
-	#self.splitter_2.setSizes( ( 200, 500) )
 
 	self.splitter_22 = QtGui.QSplitter(self.splitter_2)
 	self.splitter_22.setOrientation(QtCore.Qt.Vertical)
 
 
-	#self.splitter_222 = QtGui.QSplitter(self.splitter_22)
-	#self.splitter_222.setOrientation(QtCore.Qt.Horizontal)
-
-
-        #self.widget = QtGui.QWidget(self.splitter_2)
-        #self.widget.setObjectName("widget")
-
-        #self.vboxlayout = QtGui.QVBoxLayout(self.widget)
-        #self.vboxlayout.setSpacing(-1)
-        #self.vboxlayout.setObjectName("vboxlayout")
-
         self.listWidget = treeViewWidget( self.splitter_22 )
         
-        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Expanding)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.listWidget.sizePolicy().hasHeightForWidth())
-        
-	#self.listWidget.setSizePolicy(sizePolicy)
-        #self.listWidget.setMaximumSize(QtCore.QSize(16777215,16777215))
-        #self.listWidget.setSizeIncrement(QtCore.QSize(0,0))
-        #self.listWidget.setBaseSize(QtCore.QSize(0,0))
         self.listWidget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        #self.listWidget.setObjectName("listWidget")
         self.listWidget.setAlternatingRowColors(True)
-        #self.listWidget.setLineWidth(5)
-        #self.listWidget.setMidLineWidth(8)
         self.listWidget.setDragEnabled(True)
         self.listWidget.setDragDropOverwriteMode(True)
         self.listWidget.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
-        #self.listWidget.setAlternatingRowColors(True)
-        #self.listWidget.setIconSize(QtCore.QSize(18,-1))
-        #self.listWidget.setIndentation(23)
-        #self.listWidget.setRootIsDecorated(True)
-        #self.listWidget.setUniformRowHeights(True)
-        #self.listWidget.setSortingEnabled(True)
-        #self.listWidget.setAnimated(True)
-
-        #self.vboxlayout.addWidget( self.listWidget )
-        
-
-        #self.hboxlayout = QtGui.QHBoxLayout()
-        #self.hboxlayout.setObjectName("hboxlayout")
 
         self.splitter_222 = QtGui.QSplitter(self.splitter_22)
         self.splitter_222.setOrientation(QtCore.Qt.Horizontal)
@@ -193,57 +150,24 @@ class MainUi(object):
 
         self.QLineEdit1 = QtGui.QLineEdit( self.splitter_222 )
 
-        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,QtGui.QSizePolicy.Fixed)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.QLineEdit1.sizePolicy().hasHeightForWidth())
-        
-	#self.QLineEdit1.setSizePolicy(sizePolicy)
-        #self.QLineEdit1.setMinimumSize(QtCore.QSize(0,0))
-        #self.QLineEdit1.setMaximumSize(QtCore.QSize(16777215,16777215))
-        #self.QLineEdit1.setObjectName("QLineEdit1")
-
-        #self.splitter_222.addWidget( self.widget )
-        
         self.updateProgressBar = QtGui.QProgressBar(self.splitter_222)
         self.updateProgressBar.setValue(42);
-        #self.updateProgressBar.setSizePolicy(sizePolicy)
-        #self.updateProgressBar.setMinimumSize(QtCore.QSize(0,0))
-        #self.updateProgressBar.setMaximumSize(QtCore.QSize(16777215,16777215))
         self.updateProgressBar.hide();
         self.splitter_222.addWidget( self.updateProgressBar )
         
         self.QPushButton1 = QtGui.QPushButton(self.splitter_222)
 
-        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum,QtGui.QSizePolicy.Fixed)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.QPushButton1.sizePolicy().hasHeightForWidth())
-
-        #self.QPushButton1.setSizePolicy(sizePolicy)
         self.QPushButton1.setMinimumSize(QtCore.QSize(20,20))
         self.QPushButton1.setMaximumSize(QtCore.QSize(50,25))
-        #self.QPushButton1.setObjectName("QPushButton1")
         self.splitter_222.addWidget(self.QPushButton1)
 
 	self.splitter_2.setSizes( ( 100, 50) )
 
-        #self.vboxlayout.addLayout(self.hboxlayout)
-
         self.splitter = QtGui.QSplitter(self.splitter_2)
         
-        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Expanding)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.splitter.sizePolicy().hasHeightForWidth())
-        #self.splitter.setSizePolicy(sizePolicy)
-        
         self.splitter.setOrientation(QtCore.Qt.Vertical)
-        #self.splitter.setObjectName("splitter")
 
         self.treeWidget_2 = QtGui.QTreeWidget( self.splitter )
-        #self.treeWidget_2.setItemsExpandable(False)
-        #self.treeWidget_2.setAnimated(True)
         self.treeWidget_2.setAlternatingRowColors(True)
         self.treeWidget_2.setObjectName("treeWidget_2")
         
@@ -253,14 +177,6 @@ class MainUi(object):
 #        self.QTextBrowser1.setOpenExternalLinks(1)
 #        self.QTextBrowser1.setOpenLinks(1)
 
-        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,QtGui.QSizePolicy.Expanding)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.QTextBrowser1.sizePolicy().hasHeightForWidth())
-        #self.QTextBrowser1.setSizePolicy(sizePolicy)
-        #self.QTextBrowser1.setMinimumSize(QtCore.QSize(0,0))
-        #self.QTextBrowser1.setSizeIncrement(QtCore.QSize(4,0))
-        #self.QTextBrowser1.setBaseSize(QtCore.QSize(400,0))
         self.QTextBrowser1.setObjectName("QTextBrowser1")
 
 	self.AudioPlayer = AudioPlayer( "", self.splitter)
@@ -288,11 +204,6 @@ class MainUi(object):
         
         
         self.treeWidget_4 = QtGui.QTreeWidget(self.tab_3)
-        #self.treeWidget_4.setWindowModality(QtCore.Qt.NonModal)
-        #self.treeWidget_4.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        #self.treeWidget_4.setItemsExpandable(False)
-        #self.treeWidget_4.setSortingEnabled(True)
-        #self.treeWidget_4.setAnimated(True)
         self.treeWidget_4.setObjectName("treeWidget_4")
         
         
@@ -303,7 +214,7 @@ class MainUi(object):
         self.gridlayout.addWidget(self.tabWidget,0,0,1,1)
 
         self.splitter_3 = QtGui.QSplitter(self.tab_3)
-        #self.splitter_3.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter_3.setOrientation(QtCore.Qt.Horizontal)
 	self.gridlayout3.addWidget(self.splitter_3)
 	self.AudioPlayer_latestDownloads = AudioPlayer( "", self.splitter_3 )         
 	
@@ -314,15 +225,6 @@ class MainUi(object):
         self.tab_4.setObjectName("tab_4")    
         
         self.treeWidget_5 = QtGui.QTreeWidget( self.tab_4 )
-        #self.treeWidget_5.setWindowModality(QtCore.Qt.NonModal)
-        #self.treeWidget_5.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        #self.treeWidget_5.setItemsExpandable(False)
-        #self.treeWidget_5.setSortingEnabled(True)
-        #self.treeWidget_5.setAnimated(True)
-        #self.treeWidget_5.setObjectName("treeWidget_4")
-
-
-
 
         self.gridlayout4 = QtGui.QGridLayout( self.tab_4 )
         self.gridlayout4.setObjectName( "gridlayout4" )
@@ -330,9 +232,8 @@ class MainUi(object):
         self.tabWidget.addTab( self.tab_4,"" )
         self.gridlayout.addWidget( self.tabWidget,0,0,1,1 )
 
-
         self.splitter_4 = QtGui.QSplitter(self.tab_4)
-        #self.splitter_4.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter_4.setOrientation(QtCore.Qt.Horizontal)
 	self.gridlayout4.addWidget(self.splitter_4)
 	self.AudioPlayer_newestEpisodes = AudioPlayer( "", self.splitter_4 )
 
@@ -615,6 +516,4 @@ class MainUi(object):
         
     def app_quit(self):
         app.exit()
-    
-
 
