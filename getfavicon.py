87dcf1e096ef6202589886bf11ca2b9a5cfb6ea2
Marko Djordjevic <marko@droopia.net>
2019-02-09 21:22:22 +0100

    fix getfavicon

--- getfavicon.py
+++ getfavicon.py
@@ -1,5 +1,10 @@
-#import sgmllib
+# should use this: https://github.com/scottwernervt/favicon
+
+from html.parser import HTMLParser
 import urllib
+import urllib.request
+import http.client
+import sys
 
 def getIcoUrl(url):
     """
@@ -10,13 +15,12 @@ def getIcoUrl(url):
     """
     print("getIcoUrl(url): ")
     print(url)
-    class FaviconFinder(object):
+    class FaviconFinder(HTMLParser):
         """
         A Parser class for finding the favicon used (if specified).
         """
 
         def __init__(self, verbose=0):
-            #sgmllib.SGMLParser.__init__(self, verbose)
             self.favicon_url = None
 
         def start_link(self, attributes):
@@ -31,12 +35,15 @@ def getIcoUrl(url):
     if not url.startswith('http://') or url.startswith('https://'):
         url = 'http://%s' % url
     try:
-        site = urllib.urlopen(url)
-        contents = site.read()
+        site = urllib.request.urlopen(url)
+        contents = site.read().decode('utf-8')
 
         favicon_parser = FaviconFinder()
         favicon_parser.feed(contents)
     except:
+        e = sys.exc_info()[0]
+        print("favicon except 1: ")
+        print(e)
         pass
 
     # Another try block in case the parser throws an exception
@@ -49,69 +56,28 @@ def getIcoUrl(url):
             else:
                 imageURL = favicon_parser.favicon_url
 
-            print("imageURL:")
-            print(imageURL)
-
             return imageURL
 
         else:
             url = '/'.join(url.split('/',3)[2:])
             root_directory = url.split('/')[0]
-            favicon = httplib.HTTPConnection(root_directory)
+            favicon = http.client.HTTPConnection(root_directory)
             favicon.request('GET','/favicon.ico')
             response = favicon.getresponse()
 
             if response.status == 200:
                 return 'http://%s/favicon.ico' % root_directory
 
-            favicon = httplib.HTTPConnection('www.' + root_directory)
+            favicon = http.client.HTTPConnection('www.' + root_directory)
             favicon.request('GET','/favicon.ico')
             response = favicon.getresponse()
 
             if response.status == 200:
                 return 'http://%s/favicon.ico' % ('www.' + root_directory)
     except:
+        e = sys.exc_info()[0]
+        print("favicon except 2: ")
+        print(e)
         pass
     return None
 
-
-
-
-
-
-def getIcoUrl_OLD(url):
-    import urllib
-    import urlparse
-    import re
-    """Return the URL of the favourite icon associated with input.
-    Returns None if input isn't a valid address."""
-    try:
-        fp = urllib.urlopen(url)
-    except e:
-        print('greska')
-        if e[1] == "unknown url type":
-        # The given URL isn't a web address so adding
-        # /favicon.ico won't work. Might as well
-        # jump straight to giving up
-            return None
-#else:
-    if fp.headers['content-type'].startswith('text/html') or fp.headers['content-type'].startswith('application/xhtml+xml'):
-        data = fp.read()
-        fp.close()
-        # Try to find any links to shortcut icons
-        #ICONREGEX = "<link rel="shortcut icon" (?:.*)href=(?:['|"])(.[^'|"|\s]{0,})(?:['|"])(?:.*)>"
-        ICONREGEX = "<link rel=\"shortcut\ icon\" (?:.*?)href=(?:['|\"])(.[^'|\"|\s]{0,})(?:['|\"])(?:.*)/>"
-        results = re.search(ICONREGEX, data, re.I)
-    if results != None:
-        # Found the requested URL of the favourite icon
-        icourl = results.groups()[0]
-        # the base url is already known to be valid, so we
-        # don't need to check that the join works.
-        # If the icon's url is invalid, we'll let
-        # loadResizeImage handle the inability to retrieve it
-        return urlparse.urljoin(url, icourl)
-    else:
-        print('nema reziltata')
-        fp.close()
-        # Return the default of url/favicon.ico
-        return urlparse.urljoin(url, '/favicon.ico')
