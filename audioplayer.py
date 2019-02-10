784d6a83cb3bff1823a75981590d16b310b2e4ba
Marko Djordjevic <marko@droopia.net>
2019-02-10 01:24:39 +0100

    audioplayer cleaning

--- audioplayer.py
+++ audioplayer.py
@@ -12,7 +12,7 @@ class AudioPlayer(QtGui.QWidget):
     def __init__(self, url, parent = None):
 
         self.url = url
-        
+
         QtGui.QWidget.__init__(self, parent)
         self.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
 
@@ -24,34 +24,34 @@ class AudioPlayer(QtGui.QWidget):
         #self.play_pause.setMinimumSize(QtCore.QSize(20,20))
         #self.play_pause.setMaximumSize(QtCore.QSize(50,25))
         #self.play_pause.setIcon(QtGui.QIcon(':/icons/player_play.svg'))
-	self.play_pause.setText("Play")
+        self.play_pause.setText("Play")
         self.play_pause.clicked.connect(self.playClicked)
         self.player.stateChanged.connect(self.stateChanged)
 
         self.slider = Phonon.SeekSlider(self.player , self)
-        
+
         self.status = QtGui.QLabel(self)
         self.status.setAlignment(QtCore.Qt.AlignRight |
             QtCore.Qt.AlignVCenter)
-    
+
         self.download = QtGui.QPushButton("Download", self)
         self.download.clicked.connect(self.fetch)
-        
+
         layout = QtGui.QHBoxLayout(self)
         layout.addWidget(self.play_pause)
         layout.addWidget(self.slider)
         layout.addWidget(self.status)
         layout.addWidget(self.download)
 
-	self.setUrl("")
+        self.setUrl("")
 
     def setUrl(self, url):
-	#print url
-	self.player.setCurrentSource( Phonon.MediaSource(url) )
+        #print url
+        self.player.setCurrentSource( Phonon.MediaSource(url) )
 
 
     def enqueue(self, url):
-	self.player.enqueue( Phonon.MediaSource(url) )
+        self.player.enqueue( Phonon.MediaSource(url) )
 
 
     def playClicked(self):
@@ -59,7 +59,7 @@ class AudioPlayer(QtGui.QWidget):
             self.player.pause()
         else:
             self.player.play()
-            
+
     def stateChanged(self, new, old):
         if new == Phonon.PlayingState:
             self.play_pause.setText("Pause")
@@ -71,9 +71,9 @@ class AudioPlayer(QtGui.QWidget):
         h = time/3600
         m = (time-3600*h) / 60
         s = (time-3600*h-m*60)
-	#print '%02d:%02d:%02d'%(h,m,s)
+        #print '%02d:%02d:%02d'%(h,m,s)
         #self.status.setText('%02d:%02d:%02d'%(h,m,s))
-	#self.setWindowTitle('%02d:%02d:%02d'%(h,m,s))
+        #self.setWindowTitle('%02d:%02d:%02d'%(h,m,s))
 
     def fetch(self):
         print 'Should download %s'%self.url
